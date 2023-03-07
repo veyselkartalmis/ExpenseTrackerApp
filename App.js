ed87f195c75ddc1b4b5a69cf6a1614b53732cd52
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-03-08 02:30:26 +0300

    adding a header button & making expense items tappable

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import ManageExpense from "./screens/ManageExpense";
 
 import { GlobalStyles } from "./constants/styles";
 
+import IconButton from "./components/UI/IconButton";
+
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
@@ -25,6 +27,14 @@ function ExpensesOverview() {
                     backgroundColor: GlobalStyles.colors.primary500,
                 },
                 tabBarActiveTintColor: GlobalStyles.colors.accent500,
+                headerRight: ({ tintColor }) => (
+                    <IconButton
+                        icon="add"
+                        size={24}
+                        color={tintColor}
+                        onPress={() => {}}
+                    />
+                ),
             }}
         >
             <BottomTabs.Screen
