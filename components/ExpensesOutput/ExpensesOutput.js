db78e470c16615d735fffe2fce01fdbfeafea7f9
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-03-08 01:57:05 +0300

    working on expense list items

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -36,6 +36,36 @@ const DUMMY_EXPENSES = [
         amount: 4.99,
         date: new Date("2023-08-19"),
     },
+    {
+        id: "e6",
+        description: "Computer",
+        amount: 1099.99,
+        date: new Date("2023-01-19"),
+    },
+    {
+        id: "e7",
+        description: "Telephone Service",
+        amount: 199.99,
+        date: new Date("2023-11-20"),
+    },
+    {
+        id: "e8",
+        description: "Market",
+        amount: 267.99,
+        date: new Date("2023-10-07"),
+    },
+    {
+        id: "e9",
+        description: "Food",
+        amount: 54.99,
+        date: new Date("2023-09-17"),
+    },
+    {
+        id: "e10",
+        description: "Bills",
+        amount: 149.99,
+        date: new Date("2023-08-19"),
+    },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -55,7 +85,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 24,
+        paddingHorizontal: 24,
+        paddingTop: 24,
+        paddingBottom: 0,
         backgroundColor: GlobalStyles.colors.primary700,
     },
 });
