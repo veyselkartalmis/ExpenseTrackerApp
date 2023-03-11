7fdb5efb7a89766a18522ad3cbe7284ff262d57d
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-03-11 11:58:57 +0300

    supporting different editing modes & using route parameters

--- App.js
+++ App.js
@@ -73,7 +73,7 @@ export default function App() {
                 <Stack.Navigator
                     screenOptions={{
                         headerStyle: {
-                            backgroundColor: GlobalStyles.colors.primary50,
+                            backgroundColor: GlobalStyles.colors.primary500,
                         },
                         headerTintColor: "white",
                     }}
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,16 @@
 import { Text } from "react-native";
+import { useLayoutEffect } from "react";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+    const editedExpenseId = route.params?.expenseId;
+    const isEditing = !!editedExpenseId;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? "Edit Expense" : "Add Expense",
+        });
+    }, [navigation, isEditing]);
+
     return <Text>ManageExpense Screen</Text>;
 }
 
