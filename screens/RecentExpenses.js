4ce8021407a023522962fae9e1bdd2e7f114b228
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-03-14 21:07:35 +0300

    finishing touches

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -11,13 +11,14 @@ function RecentExpenses() {
         const today = new Date();
         const date7daysAgo = getDateMinusDays(today, 7);
 
-        return expense.date >= date7daysAgo;
+        return expense.date >= date7daysAgo && expense.date <= today;
     });
 
     return (
         <ExpensesOutput
             expenses={recentExpenses}
             expensesPeriod="Last 7 Days"
+            fallbackText="No expenses registered for the last 7 days."
         />
     );
 }
