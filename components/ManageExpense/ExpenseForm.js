3c56a505c307db17c13a770605e2f4e8cd06e2cf
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-03-18 12:11:31 +0300

    setting the form layout

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
 import Input from "./Input";
@@ -8,21 +8,27 @@ function ExpenseForm() {
 
     return (
         <View style={styles.formContainer}>
-            <Input
-                label="Amount"
-                textInputConfig={{
-                    keyboardType: "decimal-pad",
-                    onchangeText: amountChangeHandler,
-                }}
-            />
-            <Input
-                label="Date"
-                textInputConfig={{
-                    placeholder: "YYYY-MM-DD",
-                    maxLength: 10,
-                    onChangeText: () => {},
-                }}
-            />
+            <Text style={styles.title}>Your Expense</Text>
+            <View style={styles.inputsRow}>
+                <Input
+                    label="Amount"
+                    textInputConfig={{
+                        keyboardType: "decimal-pad",
+                        onchangeText: amountChangeHandler,
+                    }}
+                    style={styles.rowInput}
+                />
+                <Input
+                    label="Date"
+                    textInputConfig={{
+                        placeholder: "YYYY-MM-DD",
+                        maxLength: 10,
+                        onChangeText: () => {},
+                    }}
+                    style={styles.rowInput}
+                />
+            </View>
+
             <Input
                 label="Description"
                 textInputConfig={{
@@ -41,5 +47,23 @@ const styles = StyleSheet.create({
     formContainer: {
         backgroundColor: GlobalStyles.colors.primary800,
         padding: 24,
+        paddingTop: 40,
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "white",
+        marginVertical: 24,
+        textAlign: "center",
+    },
+
+    inputsRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+
+    rowInput: {
+        flex: 1,
     },
 });
